38f09e977764a9d25a5a7baa8d2d75546aaef0c6
Anthony Candelino <anthcandelino@gmail.com>
2024-10-10 11:29:16 -0400

    Initial Commit

---
+++ Dicey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dicey
+//
+//  Created by Anthony Candelino on 2024-10-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dicey/DiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyApp.swift
+//  Dicey
+//
+//  Created by Anthony Candelino on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct DiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
